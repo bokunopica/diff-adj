f2ae4cf748c97f2330a168f3d62c0b36e4561341
qq <qianq9720@163.com>
2023-06-09 14:29:50 +0800

    finetune

---
+++ scripts/tests/radbert-sd-finetune-sampling.py
@@ -0,0 +1,21 @@
+import torch
+from diffusers import StableDiffusionPipeline
+
+# model_id = "CompVis/stable-diffusion-v1-4"
+results_folder = "results"
+model_folder = "pretrained_models/radbert-sd-finetune"
+device = "cuda"
+
+
+
+pipe = StableDiffusionPipeline.from_pretrained(model_folder)
+pipe = pipe.to(device)
+
+prompt = "Focal consolidation at the left lung base, possibly representing aspiration or pneumonia.  Central vascular engorgement."
+# prompt = "Severe cardiomegaly is unchanged."
+
+
+for i in range(10):
+    image = pipe(prompt=prompt, height=512, width=512).images[0] 
+    image.save(f"{results_folder}/radbert-sd-finetune/demo_{'%02d'%i}.png")
+# sample_size = model.config.sample_size
